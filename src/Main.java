72b68d38e0f057f1183705478ac9c2c8140de93f
TerentevSA <terentevsa002@gmail.com>
2025-02-08 16:29:30 +0300

    nothing

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import java.lang.Math;
-
 class Main {
     public static void main(String[] args){
-        System.out.println(Integer.MAX_VALUE == Integer.MAX_VALUE + 1);
-        System.out.println(Double.MAX_VALUE == Double.MAX_VALUE + 1.0);
+
     }
 }

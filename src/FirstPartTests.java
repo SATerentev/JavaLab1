55a53457b706ba90771f2ce2aae4251ee82da5b5
TerentevSA <terentevsa002@gmail.com>
2025-02-25 02:16:07 +0300

    FirstPart completed

--- src/FirstPartTests.java
+++ src/FirstPartTests.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FirstPartTests {
@@ -103,6 +106,42 @@ public class FirstPartTests {
         assertTrue(FirstPart.secondTaskExpression(5.0, -5.0)); // на границе окружности, но вне треугольника
         assertFalse(FirstPart.secondTaskExpression(10.0000001, 0.0)); // на самом крайнем положении треугольника, чуть дальше него
         assertFalse(FirstPart.secondTaskExpression(5.0000001, 5.0000001)); // на самом крайнем положении окружности, чуть дальше нее
+    }
+
+    // endregion
+
+    // region THIRD TASK TESTS
+
+    @Test
+    void testsThirdTaskBigDecimal() {
+        // assertTrue(FirstPart.thirdTaskBigDecimal(BigDecimal.ZERO, BigDecimal.ZERO).compareTo(BigDecimal.ONE) == 0); Здесь будет ошибка из-за деления на 0
+        assertTrue(FirstPart.thirdTaskBigDecimal(BigDecimal.ONE, BigDecimal.TEN).compareTo(BigDecimal.ONE) == 0);
+        assertTrue(FirstPart.thirdTaskBigDecimal(BigDecimal.valueOf(5), BigDecimal.valueOf(-2)).compareTo(BigDecimal.ONE) == 0);
+        assertTrue(FirstPart.thirdTaskBigDecimal(BigDecimal.valueOf(-3), BigDecimal.valueOf(13)).compareTo(BigDecimal.ONE) == 0);
+        assertTrue(FirstPart.thirdTaskBigDecimal(BigDecimal.valueOf(1_000_000_000), BigDecimal.valueOf(-0.004_000_000)).compareTo(BigDecimal.ONE) == 0);
+        assertTrue(FirstPart.thirdTaskBigDecimal(new BigDecimal("12345.6789"), new BigDecimal("9876.54321")).compareTo(BigDecimal.ONE) == 0);
+        assertTrue(FirstPart.thirdTaskBigDecimal(BigDecimal.valueOf(Double.MAX_VALUE), BigDecimal.valueOf(Double.MIN_VALUE)).compareTo(BigDecimal.ONE) == 0);
+        // Последний тест показывает, что BigDecimal справляется с настолько точными числами, как Double.MAX_VALUE == 1.7976931348623157E308
+    }
+
+    @Test
+    void testsThirdTaskFloat() {
+        // assertEquals(1.0f, FirstPart.thirdTask(Float.MAX_VALUE, Float.MIN_VALUE)); Из-за переполнения, первое же умножение возвращает Infinity, т.е. NaN
+        assertEquals(1.0f, FirstPart.thirdTask(1.0000001f, 1.0000000f));
+        // assertEquals(1.0f, FirstPart.thirdTask(3.14f, 2.71f)); В этом тесте происходит ошибка из-за округления, особенность IEEE-754 e:1.0 a:1.0000001
+        assertEquals(1.0f, FirstPart.thirdTask(-7.25f, 7.25f));
+        assertEquals(1.0f, FirstPart.thirdTask(100.5f, 100.5f));
+        assertEquals(1.0f, FirstPart.thirdTask(0.00001f, 100000f));
+    }
+
+    @Test
+    void testsThirdTaskDouble() {
+        // assertEquals(1.0, FirstPart.thirdTask(Double.MAX_VALUE, Double.MIN_VALUE)); Здесь такая же ситуация, что и с float
+        assertEquals(1.0, FirstPart.thirdTask(0.00000000000001, -0.00000000000001));
+        assertEquals(1.0, FirstPart.thirdTask(2.718281828, 3.141592653));
+        assertEquals(1.0, FirstPart.thirdTask(-50.123, -50.123));
+        // assertEquals(1.0, FirstPart.thirdTask(-9999.9999, 0.000000001)); Ошибка из-за округления, e:1.0 a:0.9997558793701172
+        assertEquals(1.0, FirstPart.thirdTask(0.333333333333, 0.333333333333));
     }
 
     // endregion
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 class Main {
     public static void main(String[] args){
-        double x1 = 0;
-        double y1 = 5;
-        double x2 = 0;
-        double y2 = 0;
-        double x3 = 10;
-        double y3 = 0;
-        System.out.println(Math.abs((x1 - x3) * (y2 -y3) - (x2 - x3) * (y1 - y3)) / 2.0);
+
     }
 }
